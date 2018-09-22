8670ec779218d2e81b684903d5a3cd0a59de78f9
Jan Manuel Schlieper <jms@JansMacBookPro.local>
2018-09-22 09:48:03 +0200

    tinycolor

--- less2mjss/src/lessFunctions.ts
+++ less2mjss/src/lessFunctions.ts
@@ -1,24 +1,24 @@
-import * as Color from 'color';
-// import Color from 'tinycolor2';
-//
-// Color.prototype.alpha = function(val = false) {
+// import * as Color from 'color';
+import * as Color from 'tinycolor2';
 
-//     if (val === false) {
-//         return this.getAlpha();
-//     } else {
-//         return this.setAlpha(val);
-//     }
-// };
-// Color.prototype.hsl = Color.prototype.toHsl;
-// Color.prototype.hex = Color.prototype.toHex;
-// Color.prototype.rgb = Color.prototype.toRgb;
-// Color.prototype.toLessString = function() {
-//     return (this.alpha() < 1 ? this.toRgbString() : ('#' + this.hex()).toLowerCase());
-// };
+Color.prototype.alpha = function(val = false) {
 
-Color.prototype.toLessString = function() {
-    return (this.alpha() < 1 ? this.rgb().toString() : this.hex().toLowerCase());
+    if (val === false) {
+        return this.getAlpha();
+    } else {
+        return this.setAlpha(val);
+    }
 };
+Color.prototype.hsl = Color.prototype.toHsl;
+Color.prototype.hex = Color.prototype.toHex;
+Color.prototype.rgb = Color.prototype.toRgb;
+Color.prototype.toLessString = function() {
+    return (this.alpha() < 1 ? this.toRgbString() : ('#' + this.hex()).toLowerCase());
+};
+
+// Color.prototype.toLessString = function() {
+//     return (this.alpha() < 1 ? this.rgb().toString() : this.hex().toLowerCase());
+// };
 
 /**
  * a set of functions emulating the behaviour of lesscss (% will be assumed to be normalized to 0-1)
@@ -38,35 +38,27 @@ Color.prototype.toLessString = function() {
 export function darken(color, amount) {
     color = Color(color);
 
-    // return color.darken(parseFloat(amount)).toLessString();
+    return color.darken(parseFloat(amount)).toLessString();
 
     // scale values by 100 to get a floating point error similar to less!
     // otherwise we get rounting errors
-    color = color.hsl();
-    color.color[2] = (color.color[2] / 100 - parseFloat(amount) / 100) * 100;
 
-    return color.toLessString();
+    // color = color.hsl();
+    // color.color[2] = (color.color[2] / 100 - parseFloat(amount) / 100) * 100;
+
+    // return color.toLessString();
 }
 
 export function lighten(color, amount) {
-    // color = CoClor(color);
-    // return color.lighten(amount)
+    color = Color(color);
+
+    debugger;
+    return color.lighten(parseFloat(amount)).toLessString();
+
     return darken(color, `-${amount}`);
 }
 
-export function escape(input = '') {
-    return encodeURI(input).replace(/=/g, '%3D').replace(/:/g, '%3A').replace(/#/g, '%23').replace(/;/g, '%3B')
-            .replace(/\(/g, '%28').replace(/\)/g, '%29');
-}
 
-export function fade(color, amount) {
-    color = Color(color);
-    return color.alpha(parseFloat(amount) / 100).toLessString();
-}
-
-export function replace(string, pattern, replacement, flags = '') {
-    return string.replace(new RegExp(pattern, flags), replacement);
-}
 
 export function fadeout(color, amount) {
     color = Color(color);
@@ -78,11 +70,13 @@ export function fadeout(color, amount) {
 }
 
 export function saturate(color, amount) {
-    color = Color(color).hsl();
+    // color = Color(color).hsl();
+    color = Color(color);
 
-    color.color[1] += parseFloat(amount);
+    amount = parseFloat(amount);
+    // color.color[1] += amount;
 
-    return color.toLessString();
+    return color.saturate(amount).toLessString();
 }
 
 export function dataUri(mimetype, filePath) {
@@ -96,8 +90,28 @@ export function desaturate(color, amount) {
 
 export function spin(color, amount) {
     color = Color(color);
-    return color.rotate(parseFloat(amount)).toLessString();
+    return color.spin(parseFloat(amount)).toLessString();
+    // return color.rotate(parseFloat(amount)).toLessString();
 }
+
+export function tint(color, amount) {
+    return mix(Color({r: 255, g: 255, b: 255}), Color(color), parseFloat(amount));
+}
+
+export function mix(color, color2, amount = '50%') {
+    color = Color(color);
+    color2 = Color(color2);
+    // return color2.mix(color, parseFloat(amount) / 100).toLessString();
+    return Color.mix(color2, color, parseFloat(amount)-0.0001).toLessString();
+}
+
+export function fade(color, amount) {
+    color = Color(color);
+    return color.alpha(parseFloat(amount) / 100).toLessString();
+}
+
+
+
 
 export function floor(val) {
     return Math.floor(val) + (val.unit || '');
@@ -110,20 +124,14 @@ export function e(arg) {
     return arg && arg.replace(/"/g, '') || '';
 }
 
-export function tint(color, amount) {
-    return mix(Color({r: 255, g: 255, b: 255}), Color(color), parseFloat(amount));
+export function escape(input = '') {
+    return encodeURI(input).replace(/=/g, '%3D').replace(/:/g, '%3A').replace(/#/g, '%23').replace(/;/g, '%3B')
+            .replace(/\(/g, '%28').replace(/\)/g, '%29');
 }
 
-export function mix(color, color2, amount = '50%') {
-    color = Color(color);
-    color2 = Color(color2);
-    // debugger
-    return color2.mix(color, parseFloat(amount) / 100).toLessString();
-    // return Color.mix(color2, color, parseFloat(amount)).toLessString();
+export function replace(string, pattern, replacement, flags = '') {
+    return string.replace(new RegExp(pattern, flags), replacement);
 }
-
-// floor.passUnits = true;
-// round.passUnits = true;
 
 // export functions with mapping
 export default {
